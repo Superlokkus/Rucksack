2dde13eb3f9d6f1592584ac71d1b780adbf38bf3
Markus Klemm <markus@markusklemm.net>
2014-10-01 13:51:08 +0200

    Initial Commit

---
+++ Rucksack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rucksack
+//
+//  Created by Markus Klemm on 01.10.14.
+//  Copyright (c) 2014 net.markusklemm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
